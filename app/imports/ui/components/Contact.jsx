8f9a3fbcc7ecfb81816d18a4fe4d5fdc1ba80b96
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-10-30 13:10:22 -1000

    working on list-contacts-page-1

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Contact = ({ stuff }) => (
+/** Renders a single row in the List contact table. See pages/ListContact.jsx. */
+const Contact = ({ contact }) => (
   <tr>
-    <td>{stuff.name}</td>
-    <td>{stuff.quantity}</td>
-    <td>{stuff.condition}</td>
+    <td>{contact.name}</td>
+    <td>{contact.quantity}</td>
+    <td>{contact.condition}</td>
     <td>
-      <Link to={`/edit/${stuff._id}`}>Edit</Link>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </td>
   </tr>
 );
 
 // Require a document to be passed to this component.
 Contact.propTypes = {
-  stuff: PropTypes.shape({
+  contact: PropTypes.shape({
     name: PropTypes.string,
     quantity: PropTypes.number,
     condition: PropTypes.string,
