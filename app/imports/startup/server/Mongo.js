8bf517c77e2699f0588b44435b0bf29a98957ef9
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-10-31 10:43:12 -1000

    working on digits part3

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contact } from '../../api/contact/Contact.js';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-const addData = (data) => {
+const addStuffData = (data) => {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
+};
+
+const addContactData = (data) => {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Contact.collection.insert(data);
 };
 
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+    Meteor.settings.defaultData.forEach(data => addStuffData(data));
+  }
+}
+
+if (Contact.collection.find().count() === 0) {
+  if (Meteor.settings.defaultData) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultContacts.forEach(data => addContactData(data));
   }
 }
